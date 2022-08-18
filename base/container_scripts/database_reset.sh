3a02f7cf1ff4771a6bbd9551691e638b8e602164
James Tanner <tanner.jc@gmail.com>
2022-08-18 14:52:08 -0400

    Extensions are pre-installed.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- base/container_scripts/database_reset.sh
+++ base/container_scripts/database_reset.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# this gives us the reset_db django command
-pip show django-extensions || pip install django-extensions
-
 # the operation will error out unless all services are stopped
 ls /var/run/s6/services | egrep ^pulp | xargs -I {} s6-svc -d /var/run/s6/services/{}
 
