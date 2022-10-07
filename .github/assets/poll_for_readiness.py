7346aad69eb402185fc5d3dc0b507d1d0d11599c
David Newswanger <dnewswan@redhat.com>
2022-10-07 11:49:52 -0400

    fix poll script

--- .github/assets/poll_for_readiness.py
+++ .github/assets/poll_for_readiness.py
@@ -1,21 +1,23 @@
 import time
 
 from urllib import request
-from urllib.error import HTTPError
 import sys
 
 NUM_ATTEMPTS = 20
 WAIT_TIME = 15
 
-status_api = sys.argv[1]
-for i in range(NUM_ATTEMPTS):
-    print(f"Waiting for API to start (attempt {i+1} of {NUM_ATTEMPTS})")
-    try:
-        if request.urlopen(status_api).code == 200:
-            print(f"{status_api} online after {(i * WAIT_TIME)} seconds")
-            exit(0)
-    except HTTPError:
-        time.sleep(WAIT_TIME)
+def poll():
+    status_api = sys.argv[1]
+    for i in range(NUM_ATTEMPTS):
+        print(f"Waiting for API to start (attempt {i+1} of {NUM_ATTEMPTS})")
+        try:
+            if request.urlopen(status_api).code == 200:
+                print(f"{status_api} online after {(i * WAIT_TIME)} seconds")
+                return
+        except:
+            time.sleep(WAIT_TIME)
 
-print(f"Failed to start {status_api} after {NUM_ATTEMPTS * WAIT_TIME} seconds")
-exit(1)
+    print(f"Failed to start {status_api} after {NUM_ATTEMPTS * WAIT_TIME} seconds")
+    exit(1)
+
+poll()
