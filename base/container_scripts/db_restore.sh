a07515a7b2dabc1de1e718968514518450457079
David Newswanger <dnewswan@redhat.com>
2023-02-07 13:09:17 -0500

    Include certs in backup and restore

--- base/container_scripts/db_restore.sh
+++ base/container_scripts/db_restore.sh
@@ -10,6 +10,16 @@ echo "$SERVICES" | xargs -I {} s6-rc -d change {}
 echo "extracting $FILENAME.tar.gz to /var/lib/pulp"
 tar --overwrite -xzf /opt/oci_env/db_backup/$FILENAME.tar.gz -C /var/lib/pulp/
 
+# older backups don't have the certs dir
+if [ -d "/var/lib/pulp/certs/" ] 
+then
+    echo "restoring /etc/pulp/certs"
+    rm -rf /etc/pulp/certs/
+    mv /var/lib/pulp/certs/ /etc/pulp/certs/
+    chown -R pulp:pulp  /etc/pulp/certs/
+fi
+
+
 echo "restoring database"
 pg_restore --clean -U pulp -d pulp "/var/lib/pulp/pulp_db.backup"
 
