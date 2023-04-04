9f3178d6b5d7e95b8f669ba2638fd038a1632b41
Grant Gainey <ggainey@redhat.com>
2023-04-04 07:53:24 -0400

    Taught oci_env about the possible existence of 'performance' tests.
    See pulp_rpm/tests/performance, for example.
    
    Will install perfest_requirements if found, defaulting to
    functest_requirements if not.
    
    closes #106

---
+++ base/container_scripts/install_performance_requirements.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+declare PROJECT="$1"
+
+if [ ! -d "/src/$PROJECT/" ]
+then
+    echo "Please clone $PROJECT into ../$PROJECT/"
+    exit 1
+fi
+
+cd "/src/$PROJECT/"
+
+if [[ -f perftest_requirements.txt ]]; then
+    pip install -r perftest_requirements.txt
+elif [[ -f functest_requirements.txt ]]; then
+    pip install -r functest_requirements.txt
+fi
