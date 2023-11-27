bb435f60e5acb768e955c5ccf86de19731129e69
jerabekjiri <jerabekjirka@email.cz>
2023-11-27 15:22:21 +0100

    undo pip3
    [noissue]

--- base/container_scripts/run_python_shell.sh
+++ base/container_scripts/run_python_shell.sh
@@ -1,3 +1,4 @@
-pip3 show django-extensions || pip3 install django-extensions
+# pip3 show django-extensions || pip3 install django-extensions
+pip show django-extensions || pip install django-extensions
 
 pulpcore-manager shell_plus
