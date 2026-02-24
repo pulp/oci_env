a46bfc3957fd17c8d45e8a7fa65840a98d9ac38d
Gerrod Ubben <gerrodubben@gmail.com>
2026-02-24 12:33:45 -0500

    Improve client util functions
    Hide OCI_ENV_PATH logging behind verbose flag
    At check for TTY on exec
    Fix compose command for docker compose

--- base/init.sh
+++ base/init.sh
@@ -12,5 +12,6 @@ echo 'Defaults    secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/us
 echo 'Defaults    env_keep += "DJANGO_SETTINGS_MODULE PULP_SETTINGS XDG_CONFIG_HOME"' > /etc/sudoers.d/preserve_env
 
 # Add user pulp to sudoers so tests can use sudo
-echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/nopasswd
+# !pam_acct_mgmt is needed in containers where the pulp user lacks a /etc/shadow entry
+printf '%s\n' '%wheel ALL=(ALL) NOPASSWD: ALL' 'Defaults !pam_acct_mgmt' > /etc/sudoers.d/nopasswd
 usermod -aG wheel pulp && echo 'Adding the pulp user to the wheel group...'
